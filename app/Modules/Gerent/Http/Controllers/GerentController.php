584b8c8afec4eaff70704fef195120f8931e6ed2
eya tayari <eya.tayari@outlook.com>
2022-10-13 23:08:10 +0100

    composition-modulaire

---
+++ app/Modules/Gerent/Http/Controllers/GerentController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Modules\Gerent\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class GerentController extends Controller
+{
+
+    /**
+     * Display the module welcome screen
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function welcome()
+    {
+        return view("Gerent::welcome");
+    }
+}
---
+++ app/Modules/Gerent/Models/Gerent.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Modules\Gerent\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Gerent extends Model
+{
+    //
+}
