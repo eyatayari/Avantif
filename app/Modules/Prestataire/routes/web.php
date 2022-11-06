7e4f80cbb0bc4905ec0055d967f1c481ee13bb52
eya tayari <eya.tayari@outlook.com>
2022-11-06 23:42:14 +0100

    changes

--- app/Modules/Prestataire/routes/web.php
+++ app/Modules/Prestataire/routes/web.php
@@ -2,4 +2,4 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('prestataire', 'PrestataireController@welcome');
+Route::get('prestataire', 'PrestataireController@welcome')->name("prestataire");
