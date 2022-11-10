052e6dbd2d167e26a472fbca983b77078003b87b
eya tayari <eya.tayari@outlook.com>
2022-11-10 23:55:21 +0100

    relations_facture

--- app/Modules/Facture/Models/Facture.php
+++ app/Modules/Facture/Models/Facture.php
@@ -2,15 +2,29 @@
 
 namespace App\Modules\Facture\Models;
 
+use App\Modules\Client\Models\Client;
+use App\Modules\Gerent\Models\produit;
 use App\Modules\Prestataire\Models\Prestataire;
 use Illuminate\Database\Eloquent\Model;
 
 class Facture extends Model
 {
-    public function client(){
-        //$this->belongsTo
-    }
+    protected $fillable=[
+        'numFacture',
+        'DateFacture',
+        'mode_paiement',
+        'notes',
+        'totalFacture',
+        'prestation_id',
+        'product_id',
+        'client_id',
+    ];
     public function prestataire(){
         $this->belongsTo(Prestataire::class);
     }
+    public function client(){
+        $this->belongsTo(Client::class);
+    }
+    public function produits(){
+        $this->hasMany(produit::class);}
 }
---
+++ database/migrations/2022_11_10_224508_relations.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Relations extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+
+        Schema::table('factures', function (Blueprint $table) {
+            // relation enseignant etablissement
+
+            //relation typeEtablissement etablissement
+            $table->foreign('prestation_id')->references('id')->on('prestation');
+            $table->foreign('product_id')->references('id')->on('produits');
+            $table->foreign('client_id')->references('id')->on('clients');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
