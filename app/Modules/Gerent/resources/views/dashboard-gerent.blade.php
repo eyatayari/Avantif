2a66953e309e83ccc8c5200dd31e1d00476f28b2
samarsss <samarmediouny@gmail.com>
2022-12-02 00:22:36 +0100

    view dashboard

--- app/Modules/Gerent/resources/views/dashboard-gerent.blade.php
+++ app/Modules/Gerent/resources/views/dashboard-gerent.blade.php
@@ -18,7 +18,7 @@
         <div class="row">
 
           <!-- Sales Card -->
-          <div class="col-xxl-4 col-md-6">
+          <div class="col-xxl-4 col-md-4">
             <div class="card info-card sales-card">
 
               <div class="filter">
@@ -35,7 +35,7 @@
               </div>
 
               <div class="card-body">
-                <h5 class="card-title">Ventes <span>| Aujourd'hui</span></h5>
+                <h5 class="card-title">Ventes <span>| ce mois-ci</span></h5>
 
                 <div class="d-flex align-items-center">
                   <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
@@ -43,7 +43,7 @@
                   </div>
                   <div class="ps-3">
                     <h6>145</h6>
-                    <span class="text-success small pt-1 fw-bold">12%</span> <span class="text-muted small pt-2 ps-1">increase</span>
+                    <span class="text-success small pt-1 fw-bold">12%</span> <span class="text-muted small pt-2 ps-1">plus</span>
 
                   </div>
                 </div>
@@ -53,7 +53,7 @@
           </div><!-- End Sales Card -->
 
           <!-- Revenue Card -->
-          <div class="col-xxl-4 col-md-6">
+          <div class="col-xxl-4 col-md-4">
             <div class="card info-card revenue-card">
 
               <div class="filter">
@@ -78,7 +78,7 @@
                   </div>
                   <div class="ps-3">
                     <h6>$3,264</h6>
-                    <span class="text-success small pt-1 fw-bold">8%</span> <span class="text-muted small pt-2 ps-1">increase</span>
+                    <span class="text-success small pt-1 fw-bold">8%</span> <span class="text-muted small pt-2 ps-1">plus</span>
 
                   </div>
                 </div>
@@ -88,7 +88,7 @@
           </div><!-- End Revenue Card -->
 
           <!-- Customers Card -->
-          <div class="col-xxl-4 col-xl-12">
+          <div class="col-xxl-4 col-xl-4">
 
             <div class="card info-card customers-card">
 
@@ -106,7 +106,7 @@
               </div>
 
               <div class="card-body">
-                <h5 class="card-title">Clients <span>| Cette année</span></h5>
+                <h5 class="card-title">Clients <span>| ce mois-ci</span></h5>
 
                 <div class="d-flex align-items-center">
                   <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
@@ -114,7 +114,7 @@
                   </div>
                   <div class="ps-3">
                     <h6>1244</h6>
-                    <span class="text-danger small pt-1 fw-bold">12%</span> <span class="text-muted small pt-2 ps-1">decrease</span>
+                    <span class="text-danger small pt-1 fw-bold">12%</span> <span class="text-muted small pt-2 ps-1">moins</span>
 
                   </div>
                 </div>
@@ -128,104 +128,69 @@
        
 <div class="col-12" >
   
-    <div class="card">
-      <div class="card-body">
-        <h5 class="card-title">Nombre des prestataions</h5>
+ 
+     
 
-        <!-- Line Chart -->
-        <div id="lineChart"></div>
-
-        <script>
-          document.addEventListener("DOMContentLoaded", () => {
-            new ApexCharts(document.querySelector("#lineChart"), {
-              series: [{
-                name: "Desktops",
-                data: [10, 41, 35, 51, 49, 62, 69, 91, 148]
-              }],
-              chart: {
-                height: 350,
-                type: 'line',
-                zoom: {
-                  enabled: false
-                }
-              },
-              dataLabels: {
-                enabled: false
-              },
-              stroke: {
-                curve: 'straight'
-              },
-              grid: {
-                row: {
-                  colors: ['#f3f3f3', 'transparent'], // takes an array which will be repeated on columns
-                  opacity: 0.5
-                },
-              },
-              xaxis: {
-                categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep'],
-              }
-            }).render();
-          });
-        </script>
-        <!-- End Line Chart -->
-
-      </div>
-  
-  </div>
-
-
-</div>
           <!-- Top Selling -->
-          <div class="col-12">
+          
             <div class="card top-selling overflow-auto">
 
-              <div class="filter">
-                <a class="icon" href="#" data-bs-toggle="dropdown"><i class="bi bi-three-dots"></i></a>
-                <ul class="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
-                  <li class="dropdown-header text-start">
-                    <h6>Filter</h6>
-                  </li>
-
-                  <li><a class="dropdown-item" href="#">Aujourd'hui</a></li>
-                  <li><a class="dropdown-item" href="#">ce mois-ci</a></li>
-                  <li><a class="dropdown-item" href="#">Cette année</a></li>
-                </ul>
-              </div>
-
+              
               <div class="card-body pb-0">
-                <h5 class="card-title">Meilleures ventes <span>| Aujourd'hui</span></h5>
+                <h5 class="card-title">Meilleures ventes <span>| ce mois-ci</span></h5>
 
                 <table class="table table-borderless">
                   <thead>
                     <tr>
                       <th scope="col">Aperçu</th>
                       <th scope="col">Produit</th>
+                      <th scope="col">Marque</th>
+                      <th scope="col">Catégories</th>
                       <th scope="col">Prix</th>
-                      <th scope="col">Vendu</th>
-                      <th scope="col">Revenue</th>
+                      <th scope="col">Nombre de Ventes</th>
+                      
                     </tr>
                   </thead>
                   <tbody>
                     <tr>
-                      <th scope="row"><a href="#"><img src="assets/img/product-1.jpg" alt=""></a></th>
-                      <td><a href="#" class="text-primary fw-bold">produit1</a></td>
-                      <td>$64</td>
-                      <td class="fw-bold">124</td>
-                      <td>$5,828</td>
+                      <th scope="row"><a href="#"><img src="assets/img/Biosun-Invisible.png" alt=""></a></th>
+                      <td><a href="#" class="text-primary fw-bold">Biosun-Invisible</a></td>
+                      <td>Roncey pharma</td>
+                      <td>soins</td>
+                      <td class="fw-bold">45 €</td>
+                      <td>5600</td>
                     </tr>
                     <tr>
-                      <th scope="row"><a href="#"><img src="assets/img/product-2.jpg" alt=""></a></th>
-                      <td><a href="#" class="text-primary fw-bold">produit2</a></td>
-                      <td>$46</td>
-                      <td class="fw-bold">98</td>
-                      <td>$4,508</td>
+                      <th scope="row"><a href="#"><img src="assets/img/Huile-Capillaire.png" alt=""></a></th>
+                      <td><a href="#" class="text-primary fw-bold">Huile-Capillaire</a></td>
+                      <td>Avéne</td>
+                      <td>Coiffants</td>
+                      <td class="fw-bold">120 € </td>
+                      <td>508</td>
                     </tr>
                     <tr>
-                      <th scope="row"><a href="#"><img src="assets/img/product-3.jpg" alt=""></a></th>
-                      <td><a href="#" class="text-primary fw-bold">produit3</a></td>
-                      <td>$59</td>
-                      <td class="fw-bold">74</td>
-                      <td>$4,366</td>
+                      <th scope="row"><a href="#"><img src="assets/img/Glycerine1.png" alt="Glycerine"></a></th>
+                      <td><a href="#" class="text-primary fw-bold">Glycerine végétale</a></td>
+                      <td>Nature Bio</td>
+                      <td>Soins</td>
+                      <td class="fw-bold">14 € </td>
+                      <td>450</td>
+                    </tr>
+                    <tr>
+                      <th scope="row"><a href="#"><img src="assets/img/Dissolvant.png" alt="Glycerine"></a></th>
+                      <td><a href="#" class="text-primary fw-bold">Dissolvant</a></td>
+                      <td>Roncey pharma</td>
+                      <td>Soins</td>
+                      <td class="fw-bold">12 € </td>
+                      <td>449</td>
+                    </tr>
+                    <tr>
+                      <th scope="row"><a href="#"><img src="assets/img/Biosun-Teinte.png" alt="Glycerine"></a></th>
+                      <td><a href="#" class="text-primary fw-bold">Biosun-Teinte</a></td>
+                      <td>Nature Bio</td>
+                      <td>Soins</td>
+                      <td class="fw-bold">46 € </td>
+                      <td>449</td>
                     </tr>
                   
                   </tbody>
@@ -234,10 +199,10 @@
               </div>
 
             </div>
-          </div><!-- End Top Selling -->
+          <!-- End Top Selling -->
 
-        </div>
-      </div><!-- End Left side columns -->
+    
+   
 
       <!-- Right side columns -->
      
