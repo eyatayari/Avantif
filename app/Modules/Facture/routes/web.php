f30106e49461a04e6b3d5a377abf8190e76ff4eb
eya tayari <eya.tayari@outlook.com>
2022-12-13 21:21:48 +0100

    facture

--- app/Modules/Facture/routes/web.php
+++ app/Modules/Facture/routes/web.php
@@ -12,7 +12,7 @@ Route::get('facture/{numFacture}', 'FactureController@DisplayAddFactureTest')->n
 Route::post('store-facture','FactureController@store')->name("facture.store");
 Route::get('getProduits', 'FactureController@getProduits');
 Route::get('pdf/{numFacture}','FactureController@GenererPdf')->name('imprimer');
-
+Route::get('getPrestations', 'FactureController@getPrestations');
 /*Route::get('/test',function (){
     $facture=Facture::where("numFacture",1)->with('prestations')->first();
     //$prestation=Prestation::find(1)->with('factures')->get();
